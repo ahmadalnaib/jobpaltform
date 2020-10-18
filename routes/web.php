83b6f8aa7cb70067716c85004c9943fad0398414
ahmedhabeeb73 <alnaib888@gmail.com>
2020-10-18 19:49:50 +0200

    add mirgation model profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/* profile route*/
+Route::get('/profile',[ProfileController::class,'index'])->name('profile');
+Route::get('/profile/update',[ProfileController::class,'update'])->name('profile.update');
